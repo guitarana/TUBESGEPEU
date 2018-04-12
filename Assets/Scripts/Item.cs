2f38e4ec89d4c8b82326fec4517d422b9295af11
Gilang R.E. Guitarana <gilangguitarana@gmail.com>
2018-04-12 16:42:11 +0700

    Initial Commit

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    private TextMesh label;
+    public string value;
+
+    public enum ItemType
+    {
+        Number,
+        Operation,
+        Buff,
+        Others
+    }
+
+    public ItemType itemType = ItemType.Number;
+
+	// Use this for initialization
+	void Start () {
+
+        if (itemType == ItemType.Number)
+        {
+            int iVal = Random.Range(0, 9);
+            value = iVal.ToString();
+        }
+
+        if (itemType == ItemType.Operation)
+        {
+         //   Debug.Log("OPPPPP!!!");
+            int iVal = Random.Range(0, 3);
+            switch (iVal)
+            {
+                case 0:
+                    Debug.Log("OPPPPP!!!");
+                    value = "+";
+                    break;
+                case 1:
+                    value = "-";
+                    break;
+                case 2:
+                    value = "*";
+                    break;
+                case 3:
+                    value = "/";
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+
+        label = transform.GetChild(0).gameObject.GetComponent<TextMesh>();
+        if (label)
+            label.text = value;    
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator animator;
+    public float speed = 5;
+    public float rotationSpeed = 10;
+    public int maxInventory = 2;
+    public List<string> inventory;
+
+	// Use this for initialization
+	void Start () {
+        inventory = new List<string>();
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("Run", true);
+            Move(Vector3.forward);
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Rotate(1 * rotationSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Rotate(-1 * rotationSpeed);
+        }
+
+    }
+
+    public void Move(Vector3 dir)
+    {
+       transform.Translate(dir * speed * Time.deltaTime);
+    }
+
+    public void Rotate(float degree)
+    {
+        transform.Rotate(Vector3.up, degree * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Item>())
+        {
+            if (inventory.Count < maxInventory)
+            {
+                inventory.Add(other.gameObject.GetComponent<Item>().value);
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
